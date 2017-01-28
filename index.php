a99b39e8c996c40a58efc8e3f49032fde4c91c6a
John Dott <jkd28@pitt.edu>
2017-01-28 05:03:36 -0500

    Removed Audio only functionality from index.php; created login.php page for pharmacists, with a few bugs to be resolved; db_config for php login

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
-		<meta property="og:image" content="http://www.pitt.edu/~jkd28/img/PittSeal.png" />
 		<link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 		<script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
@@ -14,10 +13,7 @@
 		</div>
 		<div class="container-fluid">
 			<button type="button" class="btn btn-default btn-lg" style="background-color:#ff9900">
-				<span class="glyphicon glyphicon-comment" aria-hidden="true"></span>&nbsp;&nbsp;Text Chat
-			</button></br></br>
-			<button type="button" class="btn btn-default btn-lg" style="background-color:#ff9900">
-				<span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span>&nbsp;&nbsp;Audio Chat
+				<span class="glyphicon glyphicon-comment" aria-hidden="true"></span>&nbsp;&nbsp;Text  Chat
 			</button></br></br>
 			<button type="button" class="btn btn-default btn-lg" style="background-color:#ff9900">
 				<span class="glyphicon glyphicon-facetime-video" aria-hidden="true"></span>&nbsp;&nbsp;Video Chat
